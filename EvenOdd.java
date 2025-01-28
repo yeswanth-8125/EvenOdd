1c9f5c39138638e74ea43483ef7f6db7ccbb6d56
yeswanth-8125 <yeswanthkumarelluru@gmail.com>
2025-01-29 00:59:58 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int  a=16;
+        if(a%2==0){
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+    }
+}
